174351b96ba602309afa762bd7fe352371028b9e
lixiaolei19821218 <735534185@qq.com>
2013-03-28 17:16:46 +0800

    add

---
+++ Control Fun/BIDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BIDAppDelegate.h
+//  Control Fun
+//
+//  Created by 李 潇磊 on 13-3-28.
+//  Copyright (c) 2013年 李 潇磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BIDViewController;
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BIDViewController *viewController;
+
+@end
---
+++ Control Fun/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Control Fun
+//
+//  Created by 李 潇磊 on 13-3-28.
+//  Copyright (c) 2013年 李 潇磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
